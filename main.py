42a2dc14f32dbeb57db22edc8f8a348437e583a3
JM <j-marks@live.co.uk>
2020-02-09 18:24:39 +0000

    changed successobject to parent class, created hitter child object

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ class DiceRoller:
         print("Dice roller created")
         self.myMaxValue = maxValue
 
-    def __call__(self, diceToRoll = 1):
+    def __call__(self, diceToRoll=1):
         if diceToRoll == 1:
             return ceil(self.myMaxValue * rand.random())
         dice_results = []
@@ -19,69 +19,81 @@ class DiceRoller:
         return dice_results
 
 
-hitTypes = ['hit', 'miss', 'wound', 'mortal']
-hitTypesEnum = enumerate(hitTypes)
-woundTypes = ['wound', 'fail', 'mortal']
-
-
 class SuccessObject:
     def __init__(self, successRoll):
-        self.__mySuccessRoll = successRoll
-        self.myHitModifier = 0
+        self.mySuccessRoll = successRoll
+        self.myDiceModifier = 0
         self.myRerollType = 'none'
         self.myDiceReRoller = DiceRoller(6)
-        self.myAutoWoundModified = 100
-        self.myAutoWoundUnmodified = 100
         self.myMortalWoundModified = 100
         self.myMortalWoundUnmodified = 100
         self.myExplodingHitsModified = []
         self.myExplodingHitsUnmodified = []
+        print("Success object created")
+
+    def _explodingDice(self, value):
+        # This function handles cases where a success generates multiple successes, e.g. Death to the false emperor
+        output = []
+        for i in self.myExplodingHitsModified:  # A for loop is used because you can stack these modifiers
+            if value + self.myDiceModifier >= i:
+                output.append('success')
+        for i in self.myExplodingHitsUnmodified:
+            if value >= i:
+                output.append('success')
+        return output
+
+    def _mortalWounds(self, value):
+        # this function handles cases where a certain value generates additional hits in addition to its normal result
+        output = []
+        if value + self.myDiceModifier >= self.myMortalWoundModified:
+            output.append('mortal')
+        if value >= self.myMortalWoundUnmodified:
+            output.append('mortal')
+        return output
+
+    def _applyReRoll(self, value):
+        if self.myRerollType != 'none':
+            # This is done so that you cant keep re rolling failed hits when the function calls itself
+            newDiceRoll = self.myDiceReRoller()
+            if (self.myRerollType == 'hits'):
+                if newDiceRoll + self.myDiceModifier >= self.mySuccessRoll:
+                    return newDiceRoll
+            if (self.myRerollType == 'failedhits' and value < self.mySuccessRoll):
+                if newDiceRoll >= self.mySuccessRoll:
+                    return newDiceRoll
+            if (self.myRerollType == 'ones' and value == 1):
+                if newDiceRoll + self.myDiceModifier >= self.mySuccessRoll:
+                    return newDiceRoll
+    def _doISucceed(self, value):
+        if value + self.myDiceModifier >= self.mySuccessRoll and value != 1:
+            return ['success']
+        else:
+            return ['fail']
+
+
+class Hitter(SuccessObject):
+    def __init__(self, successRoll):
+        super().__init__(successRoll)
+        self.myAutoWoundModified = 100
+        self.myAutoWoundUnmodified = 100
         self.myDestroyerMissile = False
-        self.myAutoHit = False
+        self.myAutoSuccess = False
+        self.myAutoWoundModified = 100
+        self.myAutoWoundUnmodified = 100
 
     def __call__(self, value):
-        unModifiedValue = value
-        modifiedValue = value + self.myHitModifier
+        modifiedValue = value + self.myDiceModifier
         output = []
-        if self.myAutoHit == True:
+        if self.myAutoSuccess:
             output.append('success')
             return output
-        if modifiedValue >= self.__mySuccessRoll and value != 1:
-            if modifiedValue >= self.myAutoWoundModified:
-                output.append('wound')
-                return output
-            if unModifiedValue >= self.myAutoWoundUnmodified:
-                output.append('wound')
-            if modifiedValue >= self.myMortalWoundModified:
-                output.append('mortal')
-            if unModifiedValue >= self.myMortalWoundUnmodified:
-                output.append('mortal')
-            if self.myDestroyerMissile:  # Effects which do D3 mortal wounds on hit, e.g. tau destroyer missiles
-                num_mortal_wounds = ceil(self.myDiceReRoller() / 2)  # convert from D6 to D3
-                output += (['mortal'] * num_mortal_wounds)
-            for i in self.myExplodingHitsModified:
-                if modifiedValue >= i:
-                    output.append('success')
-            for i in self.myExplodingHitsUnmodified:
-                if unModifiedValue >= i:
-                    output.append('success')
-            output.append('success')  # this is the success for passing the roll
-        else:
-            print(value)
-            if self.myRerollType != 'none':
-                # This is done so that you cant keep re rolling failed hits when the function calls itself
-                newDiceRoll = self.myDiceReRoller()
-                if (self.myRerollType == 'hits'):
-                    if newDiceRoll + self.myHitModifier >= self.__mySuccessRoll:
-                        output += self.__call__(newDiceRoll)
-                if (self.myRerollType == 'failedhits'):
-                    if newDiceRoll >= self.__mySuccessRoll:
-                        output += self.__call__(newDiceRoll)
-                if (self.myRerollType == 'ones' and value == 1):
-                    if newDiceRoll + self.myHitModifier >= self.__mySuccessRoll:
-                        output += self.__call__(newDiceRoll)
-            else:
-                output.append('fail')
+        if modifiedValue >= self.myAutoWoundModified:
+            output.append('wound')
+            return output
+        output += self._explodingDice(value)
+        output += self._mortalWounds(value)
+        output += self._doISucceed(value)
+        if output
         return output
 
 
@@ -89,15 +101,16 @@ aDiceRoller = DiceRoller(6)
 
 diceOut = aDiceRoller(200000)
 print(diceOut)
-myHitter = SuccessObject(3)
-#myHitter.myExplodingHitsUnmodified = [6, 6]
-myHitter.myRerollType = 'hits'
+myHitter = Hitter(3)
+myHitter.myExplodingHitsUnmodified = [6, 6]
+#myHitter.myRerollType = 'hits'
 suc = 0
 fai = 0
 for j in diceOut:
-    if myHitter(j) == ['success']:
-        suc += 1
+    a = myHitter(j)
+    if 'success' in a:
+        suc += len(a)
     else:
         fai += 1
 
-print("failures: " + str(fai) + " successes: " + str(suc) + " success rate: " + str(100*suc/200000) + "%")
+print("failures: " + str(fai) + " successes: " + str(suc) + " success rate: " + str(100 * suc / 200000) + "%")
