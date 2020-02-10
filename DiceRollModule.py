62d3a99bf4387d6a408bfd164a5960ba03933ce5
JM <j-marks@live.co.uk>
2020-02-10 23:39:02 +0000

    Updated Wounder, needs testing

--- DiceRollModule.py
+++ DiceRollModule.py
@@ -1,5 +1,6 @@
 import random as rand
 from math import ceil
+from math import floor
 
 
 # not working:
@@ -7,7 +8,6 @@ from math import ceil
 
 class DiceRoller:
     def __init__(self, maxValue):
-        print("Dice roller created")
         self.myMaxValue = maxValue
 
     def __call__(self, diceToRoll=1):
@@ -19,25 +19,27 @@ class DiceRoller:
         return dice_results
 
 
+class DamageObject:
+    def __init__(self, type, damage=1, ap=0):
+        self.myType = type
+        self.myDamage = damage
+        self.myAp = ap
+
+
 class SuccessObject:
     def __init__(self, successRoll):
         self.mySuccessRoll = successRoll
         self.myDiceModifier = 0
         self.myRerollType = 'none'
-        self.myMortalWoundModified = 100
-        self.myMortalWoundUnmodified = 100
-        self.myExplodingHitsModified = []
-        self.myExplodingHitsUnmodified = []
         print("Success object created")
 
-    def _doesItExplode(self, value):
-        # This function handles cases where a success generates multiple successes, e.g. Death to the false emperor
-        output = []
-        if value + self.myDiceModifier >= self.myExplodingHitsModified[0]:
-            output.append(self.myExplodingHitsModified[1])
-        if value >= self.myExplodingHitsUnmodified[0]:
-            output.append(self.myExplodingHitsUnmodified[1])
-        return output
+    def _doesItExplode(self, diceValue, diceRequirment, isModified, bonusValue):
+        # This function is a generic function to be called when a bonus occurs on a specific value, e.g. exploding 6s
+       if isModified:
+           diceValue = diceValue - self.myDiceModifier
+       if diceValue >= diceRequirment:
+           return bonusValue
+       return 0
 
     def _diceToNum(self, value):
         # This converts dice rolls D6, 3D6 etc and gives a integer number out
@@ -54,19 +56,11 @@ class SuccessObject:
         else:
             return int(value)
 
-    def _mortalWounds(self, value):
-        # this function handles cases where a certain value generates additional hits in addition to its normal result
-        output = []
-        if value + self.myDiceModifier >= self.myMortalWoundModified:
-            output.append('mortal')
-        if value >= self.myMortalWoundUnmodified:
-            output.append('mortal')
-        return output
-
     def _applyReRoll(self, value):
         if self.myRerollType != 'none':
             # This is done so that you cant keep re rolling failed hits when the function calls itself
-            newDiceRoll = self.myDiceReRoller()
+            tempDiceRoller = DiceRoller(6)
+            newDiceRoll = tempDiceRoller()
             if self.myRerollType == 'hits':
                 if newDiceRoll + self.myDiceModifier >= self.mySuccessRoll:
                     return newDiceRoll
@@ -79,7 +73,7 @@ class SuccessObject:
             return None
 
     def _doISucceed(self, value):
-        if value + self.myDiceModifier >= self.mySuccessRoll and value != 1:
+        if value + self.myDiceModifier >= self.mySuccessRoll and not value <= 1:
             return True
         else:
             return False
@@ -92,10 +86,16 @@ class Hitter(SuccessObject):
         self.myAutoWoundUnmodified = 100
         self.myDestroyerMissile = False
         self.myAutoSuccess = False
-        self.myAutoWoundModified = 100
-        self.myAutoWoundUnmodified = 100
+        self.myAutoWound = 100
+        self.myAutoWoundIsModified = False
+        self.myMortalWound = []
+        self.myMortalWoundIsModified = False
+        self.myExplodingHits = []
+        self.myExplodingHitsIsModified = False
 
     def __call__(self, value):
+        # this function returns an array of strings, possible results are 'fail', 'success', 'mortal or 'wound' depending
+        # on the dice roll, an array is returned because you can generate more hits then dice in
         modifiedValue = value + self.myDiceModifier
         output = []
         if self.myAutoSuccess:
@@ -104,13 +104,88 @@ class Hitter(SuccessObject):
         if modifiedValue >= self.myAutoWoundModified:
             output.append('wound')
             return output
-        output += ['success']*self.doesItExplode(value)
-        output += self._mortalWounds(value)
+        if len(self.myExplodingHits) != 0:
+            output += ['success']*self._doesItExplode(value, self.myExplodingHits[0],
+                                                        self.myExplodingHitsIsModified, self.myExplodingHits[1])
+        if len(self.myMortalWound) != 0:
+            output += ['success'] * self._doesItExplode(value, self.myMortalWound[0],
+                                                        self.myExplodingHitsIsModified, self.myMortalWound[1])
         if self._doISucceed(value):
             output.append('success')
         else:
             newDice = self._applyReRoll(value)
             if newDice is not None:
                 output += self.__call__(newDice)
+            else:
+                output.append('fail')
         return output
+
+
+class Wounder(SuccessObject):
+    def __init__(self, strength, toughness, baseDamage, baseAp):
+        if strength == toughness:
+            successRoll = 4
+        elif floor(strength / 2) >= toughness:
+            successRoll = 2
+        elif strength > toughness:
+            successRoll = 3
+        elif floor(toughness / 2) >= strength:
+            successRoll = 6
+        else:
+            successRoll = 5
+        super().__init__(successRoll)
+        self.myStrength = strength
+        self.myToughness = toughness
+        self.myBaseDamage = baseDamage
+        self.myBaseAp = baseAp
+        self.myExplodingWounds = []
+        self.myExplodingWoundsIsModified = False
+        self.myRending = []
+        self.myRendingIsModified = False
+        self.myExplodingDamage = []
+        self.myExplodingDamageIsModified = False
+        self.myMortalWounds = []
+        self.myMortalWoundsIsModified = False
+        print("Wounder created")
+
+    def __generateDamageObject(self, type, bonusDamage=0, bonusAp=0):
+        output = []
+        if type == 'mortal':
+            output += [DamageObject('mortal')]*bonusDamage
+        else:
+            output.append(DamageObject(type, self.myBaseDamage+bonusDamage, self.myBaseAp+bonusAp))
+        return output
+
+    def __call__(self, diceValue, hitType):
+        output = []
+        if hitType == 'mortal':
+            output += self.__generateDamageObject('mortal')
+            return output
+        if hitType == 'wound':
+            output += self.__generateDamageObject('normal')
+            return output
+        if len(self.myRending) != 0:
+            output += self.__generateDamageObject('normal', self.myBaseDamage, self._doesItExplode(diceValue,
+                                                       self.myRending[0], self.myRendingIsModified, self.myRending[1]))
+            return output
+        if len(self.myExplodingDamage) != 0:
+            bonusDamage = self._diceToNum(self._doesItExplode(diceValue, self.myExplodingDamage[0],
+                                                       self.myExplodingDamageIsModified, self.myExplodingDamage[1]))
+            output += self.__generateDamageObject('normal', self.myBaseDamage + bonusDamage)
+            return output
+        if len(self.myMortalWounds) != 0:
+            numMortalWounds = self._diceToNum(self._doesItExplode(diceValue, self.myMortalWounds[0],
+                                                       self.myMortalWoundsIsModified, self.myMortalWounds[1]))
+            output += self.__generateDamageObject('mortal', numMortalWounds)
+        if len(self.myExplodingWounds) != 0:
+            output +=  self.__generateDamageObject('normal') * self._doesItExplode(diceValue, self.myExplodingWounds[0],
+                                                        self.myExplodingWoundsIsModified, self.myExplodingWounds[1])
+        if self._doISucceed(diceValue):
+            output += self.__generateDamageObject('normal')
+        else:
+            newDice = self._applyReRoll(diceValue)
+            if newDice is not None:
+                output += self.__call__(newDice)
+        return output
+
 
--- main.py
+++ main.py
@@ -1,70 +1,26 @@
 from DiceRollModule import *
-from math import floor
 
 
-class DamageObject:
-    def __init__(self, type, damage=1, ap=0):
-        self.myType = type
-        self.myDamage = damage
-        self.myAp = ap
 
-
-class Wounder(SuccessObject):
-    def __init__(self, strength, toughness, baseDamage, baseAp):
-        if strength == toughness:
-            successRoll = 4
-        elif floor(strength / 2) >= toughness:
-            successRoll = 2
-        elif strength > toughness:
-            successRoll = 3
-        elif floor(toughness / 2) >= strength:
-            successRoll = 6
-        else:
-            successRoll = 5
-        super().__init__(successRoll)
-        self.myStrength = strength
-        self.myToughness = toughness
-        self.myBaseDamage = baseDamage
-        self.myBaseAp = 0
-        self.myRending = []
-        self.myExplodingWoundsModifed = []
-        self.myMortalWoundsModified = []
-        print("Wounder created")
-
-        def __generateDamageObject(self, type, bonusAp=0, bonusDamage=0):
-            output = []
-            if type == 'mortal':
-                output += [DamageObject('mortal')]*bonusDamage
-            else:
-                output.append(DamageObject(type, self.myBaseDamage+bonusDamage, self.myBaseAp+bonusAp))
-            return output
-
-        def __call__(self, value, hitType):
-            output = []
-            if hitType == 'mortal':
-                output.append(DamageObject('mortal'))
-                return output
-            #exploding wounds
-            #exploding mortals
 
 
 aDiceRoller = DiceRoller(6)
 
-diceOut = aDiceRoller(10)
+diceOut = aDiceRoller(200000)
 
 myHitter = Hitter(3)
-myHitter.myDiceModifier = 0
-myHitter.myExplodingHitsUnmodified = [6, 6, 6]
-myHitter.myMortalWoundUnmodified = 5
-myHitter.myRerollType = 'hits'
+myHitter.myDiceModifier = -1
+#myHitter.myExplodingHits = [6]
+#myHitter.myMortalWoundUnmodified = 5
+myHitter.myRerollType = 'ones'
 suc = 0
 fai = 0
 for j in diceOut:
     a = myHitter(j)
+    print(a)
     if 'success' in a:
         suc += len(a)
     else:
         fai += 1
 
 print("failures: " + str(fai) + " successes: " + str(suc) + " success rate: " + str(100 * suc / 200000) + "%")
-woundTest = Wounder(1, 4, 2)
