bab4dad088ac9ba3769e74fc0f60c2c7c6f36543
J-Mar <j-marks@live.co.uk>
2020-02-13 21:40:54 +0000

    added more GUI stuff

--- gui.py
+++ gui.py
@@ -1,18 +1,50 @@
 import tkinter as tk
+from tkinter import ttk
 import seaborn as sns
 
-root = tk.Tk()
-frameWidth = 300
-frameHeight = 500
-label = tk.Label(root, text="label")
-label.pack()
+windowWidth = 1200
+windowHeight = 800
+graphFrameHeight = 800
+graphFrameWidth = 800
+tabFrameWidth = 400
+tabFrameHeight=800
 
-hitterFrame = tk.Frame(root,width=frameWidth, height=frameHeight)
-bsEntry = tk.Entry(hitterFrame).pack()
-hitModifierEntry = tk.Entry(hitterFrame).pack()
-reRollEntry = tk.Entry(hitterFrame).pack()
-numHitsEntry = tk.Entry(hitterFrame).pack()
-hitterFrame.pack()
-wounderFrame = tk.Frame(root, width=frameWidth, height=frameHeight)
+app = tk.Tk()
+app.title("Warhammer dice roller")
 
-root.mainloop()
+class Frame:
+    def __init__(self):
+        self.myFrame = tk.Frame()
+
+
+profileWindowTabControl = ttk.Notebook(app)
+attackerTab = tk.Frame(profileWindowTabControl)
+balisticSkillLabel = tk.Label(attackerTab, text="Balistic skill:")
+balisticSkillLabel.grid(row=0, column=0)
+balisticSkillEntry = tk.Entry(attackerTab)
+balisticSkillEntry.grid(row=0,column=1)
+reRollLabel = tk.Label(attackerTab, text="Re roll:")
+reRollLabel.grid(row=1,column=0)
+reRollCombo = ttk.Combobox(attackerTab, values=['ones', 'hits', 'failed hits'])
+reRollCombo.grid(row=1,column=1)
+hitModifierLabel = tk.Label(attackerTab, text="Hit modifier:")
+hitModifierLabel.grid(row=2,column=0)
+hitModifierEntry = tk.Entry(attackerTab)
+hitModifierEntry.grid(row=2,column=1)
+attackerTab.pack()
+defenderTab = tk.Frame(profileWindowTabControl)
+profileWindowTabControl.add(attackerTab, text="Attacker")
+profileWindowTabControl.add(defenderTab, text="Defender")
+
+resultWindowTabControl = ttk.Notebook(app)
+deadModelsGraphTab = tk.Frame(resultWindowTabControl)
+damageReceivedGraphTab = tk.Frame(resultWindowTabControl)
+statisticsTab = tk.Frame(resultWindowTabControl)
+resultWindowTabControl.add(deadModelsGraphTab, text="Lost models")
+resultWindowTabControl.add(damageReceivedGraphTab, text="Damage done")
+resultWindowTabControl.add(statisticsTab, text="Statistics")
+
+profileWindowTabControl.pack()
+resultWindowTabControl.pack()
+
+app.mainloop()
